e5b7661e294bc69151de3aff83dba713c1c8a092
Imad Baber <imadbaber@yahoo.com>
2017-12-02 21:57:33 -0500

    initialize

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers 
+(
+	id int AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(50),
+    devoured BOOLEAN DEFAULT false,
+    date TIMESTAMP,
+    PRIMARY KEY (id)
+);
+    
+    
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('CHICKEN BURGER',FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('CHEESE BURGER',FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('TURKEY BURGER',FALSE);
+
+SELECT * FROM burgers; 
